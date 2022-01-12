096a11ec3043a423f5c897e9e4583cd71cbd1871
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-11 20:53:46 -0500

    commit tree 6fb7ecde38bd56c614ada2d65e4cd1152d9c3a4c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName ='bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer='not bob';
+}
+
+var bestCustomer;
+
+function overwriteBestCustomer(){
+    return bestCustomer='maybe bob'
+}
+
+const leastFavoriteCustomer='karen'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer='susan'
+    
+}
